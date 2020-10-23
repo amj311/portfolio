e0c52acba5e531b33c8d34d9ace5ac72f0078a35
amj311 <amjudd315@gmail.com>
2020-10-23 00:25:14 -0600

    added logo svg and vue script to load portfolio data

--- index.js
+++ index.js
@@ -2,8 +2,26 @@ const app = new Vue({
     el: "#app",
 
     data: {
+        dataIsLoading: true,
+        portfolioData: null,
         waveHtml: `<svg viewBox="0 0 500 50" preserveAspectRatio="none">
                         <path d="M 0 20 L 0 50 L 1000 50 L 1000 20 Q 923 5 858 5 C 793 7 700 25 637 27 Q 560 32 500 20 Q 423 5 358 5 C 293 7 200 25 137 27 Q 60 32 0 20 Z"/>
                     </svg>`,
+    },
+
+    beforeMount() {
+        this.getData();
+    },
+    
+    methods: {
+        async getData() {
+            let data = await fetch("portfolio-data.json").then(
+                res => res.json()
+            )
+            console.log(data)
+            this.portfolioData = data;
+            this.dataIsLoading = false;
+        }
     }
+
 })
