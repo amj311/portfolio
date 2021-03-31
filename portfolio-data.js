77932ad400254425ccffd93c5208fd2c784be8a1
amj311 <amjudd315@gmail.com>
2021-03-31 16:51:49 -0600

    updated portfolio data

--- portfolio-data.js
+++ portfolio-data.js
@@ -61,7 +61,7 @@ const portfolioData = {
         {
             "title": "PDF Viewer",
             "summary": "A browser tool for inspecting large and complex PDF files. Part of ongoing research.",
-            "stack": ["vue","ts","node"],
+            "stack": ["python","opencv","vue","ts","node"],
             "previewUrl": "https://portfolio.simplyoliveapps.com/assets/pdfviewer.gif"
         }
     ]   
